aee93100c2adbc93f8a1042aecbf90a416c9a034
davutozcan <davutozcan87@gmail.com>
2019-04-10 22:54:14 +0300

    first components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ClipListComponent } from './clip-list/clip-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ClipListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/clip-list/clip-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clip-list',
+  templateUrl: './clip-list.component.html',
+  styleUrls: ['./clip-list.component.css']
+})
+export class ClipListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
