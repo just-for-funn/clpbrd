a6be91bce00a52923197a23bb5b6a7424878a346
davutozcan <davutozcan87@gmail.com>
2019-05-09 03:13:15 +0300

    filter mechanism working

--- src/app/clip-list/clip-list-view-model.ts
+++ src/app/clip-list/clip-list-view-model.ts
@@ -1,18 +1,27 @@
 import { ClipModel } from '../services/clip-service';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 export class ClipListViewModel{
   
-    public clips: ClipModel [] = [];
+    private clips: ClipModel [] = [];
 
-    private filteredData:ClipModel[] = [];
+    public filteredData:ClipModel[] = [];
     public filter:string = "";
+    private filter$ = new Subject<string>();
+
+    constructor(){
+        this.filter$.pipe(debounceTime(250)).subscribe(filter =>this.doFilter(filter));
+    }
 
     public onFilterChange(): void {
         console.log("Filter changed", this.filter);
+        this.filter$.next(this.filter);
     }
 
     bindData(rows: ClipModel[]): void {
         this.clips = rows.sort((a, b) => b.rowNumber - a.rowNumber);
+        this.doFilter(this.filter);
     }
 
     getMaxRow() {
@@ -21,5 +30,12 @@ export class ClipListViewModel{
 
     appendFront(cm: ClipModel) {
         this.clips.unshift(cm);
+    }
+
+    doFilter(filter:string){
+        if(filter && filter.length >0)
+            this.filteredData =  this.clips.filter(clip => clip.value.toLowerCase().includes(filter));
+        else 
+            this.filteredData = this.clips;
     }
 }
